0238f76b12f2f8bd68c9c56467f81d420171ff1c
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2020-12-27 14:47:19 +0100

    Typescript type parameters

--- index.ts
+++ index.ts
@@ -12,11 +12,15 @@ axios.get(url).then(response => {
   const todo = response.data as Todo;
   const id = todo.id;
   const title = todo.title;
-  const finished = todo.completed;
+  const completed = todo.completed;
 
+  logTodo(id, title, completed);
+})
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     The Todo with ID: ${id}
     Has a title of : ${title}
-    Is it finished? ${finished}
+    Is it finished? ${completed}
   `)
-})
+}
